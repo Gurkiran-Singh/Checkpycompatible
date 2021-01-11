421c3e24062e5484fc10673805336dd2950b716e
root <root@ip-172-31-44-155.us-east-2.compute.internal>
2021-01-11 19:29:12 +0000

    s3

--- s3.py
+++ s3.py
@@ -9,7 +9,7 @@ s3filePath = 'serverless/checkpycompatible/dev/1610129844597-2021-01-08T18:17:24
 s3 = boto3.resource('s3')
 s3.meta.client.download_file('bucketpy', 'serverless/checkpycompatible/dev/1610129844597-2021-01-08T18:17:24.597Z/checkpycompatible.zip', '//var/lib/jenkins/workspace/checkaws/checkpycompatible.zip')
 
-with zipfile.ZipFile('filePath/checkpycompatible.zip', 'r') as zip_ref:
+with zipfile.ZipFile('//var/lib/jenkins/workspace/checkaws//checkpycompatible.zip', 'r') as zip_ref:
     zip_ref.extractall('//var/lib/jenkins/workspace/checkaws//')
 
 if os.path.exists(filePath):
