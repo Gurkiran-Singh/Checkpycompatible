9975008850673f6ff4bf67a00745f470cf1f5453
root <root@ip-172-31-44-155.us-east-2.compute.internal>
2021-01-12 20:03:50 +0000

    s3

--- s3.py
+++ s3.py
@@ -12,13 +12,13 @@ prefix = 'serverless/checkpycompatible/dev/1610129844597-2021-01-08T18:17:24.597
 
 s3.meta.client.download_file('bucketpy', prefix , '//var/lib/jenkins/workspace/checkaws/checkpycompatible.zip')
 
-with zipfile.ZipFile('//var/lib/jenkins/workspace/checkaws//checkpycompatible.zip', 'r') as zip_ref:
-    zip_ref.extractall('//var/lib/jenkins/workspace/checkaws//')
+with zipfile.ZipFile(filePath + '/checkpycompatible.zip', 'r') as zip_ref:
+    zip_ref.extractall(filePath)
 
 if os.path.exists(filePath):
     os.remove( filePath +  '/checkpycompatible.zip')
-    #os.remove('//var/lib/jenkins/workspace/checkaws//package-lock.json')
-    #shutil.rmtree('//var/lib/jenkins/workspace/checkaws//node_modules')
+    os.remove( filePath + '/package-lock.json')
+    shutil.rmtree( filePath + '/node_modules')
 
 else:
     print('Cannot delete the files')
