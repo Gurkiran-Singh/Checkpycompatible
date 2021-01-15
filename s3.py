2f7d93083e9799156bfa48d136b08cc63ddae39c
root <root@ip-172-31-44-155.us-east-2.compute.internal>
2021-01-15 17:40:54 +0000

    s3

--- s3.py
+++ s3.py
@@ -3,25 +3,27 @@ import os
 import zipfile
 import shutil
 
-filePath = '//var/lib/jenkins/workspace/checkaws';
-
+filePath = '//var/lib/jenkins/workspace/checkaws'
 
 s3 = boto3.resource('s3')
-prefix = 'serverless/checkpycompatible/dev/1610129844597-2021-01-08T18:17:24.597Z/checkpycompatible.zip' 
+prefix = \
+    'serverless/checkpycompatible/dev/1610129844597-2021-01-08T18:17:24.597Z/checkpycompatible.zip'
 
+s3.meta.client.download_file('bucketpy', prefix, filePath
+                             + '/checkpycompatible.zip')
 
-s3.meta.client.download_file('bucketpy', prefix , filePath + '/checkpycompatible.zip')
-
-with zipfile.ZipFile(filePath + '/checkpycompatible.zip', 'r') as zip_ref:
+with zipfile.ZipFile(filePath + '/checkpycompatible.zip', 'r') as \
+    zip_ref:
     zip_ref.extractall(filePath)
 
 if os.path.exists(filePath):
-    os.remove( filePath +  '/checkpycompatible.zip')
-    os.remove( filePath + '/package-lock.json')
-    shutil.rmtree( filePath + '/node_modules')
-
+    os.remove(filePath + '/checkpycompatible.zip')
+    os.remove(filePath + '/package-lock.json')
+    shutil.rmtree(filePath + '/node_modules')
 else:
-    print('Cannot delete the files')
 
-#os.system('sls deploy')
+    print 'Cannot delete the files'
+
+# os.system('sls deploy'
+
 
